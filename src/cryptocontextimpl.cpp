c65226020f9eee22e35bd3c1293066761064d8a0
Michael Schlottke-Lakemper <michael@sloede.com>
2024-01-19 14:44:47 +0100

    Wrap more `EvalSub` methods

--- src/cryptocontextimpl.cpp
+++ src/cryptocontextimpl.cpp
@@ -52,9 +52,32 @@ void wrap_CryptoContextImpl(jlcxx::Module& mod) {
             static_cast<lbcrypto::Ciphertext<lbcrypto::DCRTPoly>
                         (WrappedT::*)(double,
                                       lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>) const>(&WrappedT::EvalAdd));
+
+        // EvalSub
+        // ConstCiphertext - ConstCiphertext
         wrapped.method("EvalSub",
             static_cast<lbcrypto::Ciphertext<lbcrypto::DCRTPoly>
                         (WrappedT::*)(lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>,
+                                      lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>) const>(&WrappedT::EvalSub));
+        // ConstCiphertext - ConstPlaintext
+        wrapped.method("EvalSub",
+            static_cast<lbcrypto::Ciphertext<lbcrypto::DCRTPoly>
+                        (WrappedT::*)(lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>,
+                                      lbcrypto::ConstPlaintext) const>(&WrappedT::EvalSub));
+        // ConstPlaintext - ConstCiphertext
+        wrapped.method("EvalSub",
+            static_cast<lbcrypto::Ciphertext<lbcrypto::DCRTPoly>
+                        (WrappedT::*)(lbcrypto::ConstPlaintext,
+                                      lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>) const>(&WrappedT::EvalSub));
+        // ConstCiphertext - double
+        wrapped.method("EvalSub",
+            static_cast<lbcrypto::Ciphertext<lbcrypto::DCRTPoly>
+                        (WrappedT::*)(lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>,
+                                      double) const>(&WrappedT::EvalSub));
+        // double - ConstCiphertext
+        wrapped.method("EvalSub",
+            static_cast<lbcrypto::Ciphertext<lbcrypto::DCRTPoly>
+                        (WrappedT::*)(double,
                                       lbcrypto::ConstCiphertext<lbcrypto::DCRTPoly>) const>(&WrappedT::EvalSub));
 
         // EvalMult
