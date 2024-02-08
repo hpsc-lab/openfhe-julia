8e8acf03ae6f50258dbe92b84625a475bf20a3c8
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-02-08 12:04:34 +0200

    Update test/test.jl
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- test/test.jl
+++ test/test.jl
@@ -11,8 +11,8 @@ using Test
 
     x = [0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0]
     @show ptxt = MyOpenFHE.MakeCKKSPackedPlaintext(cc[], MyOpenFHE.CxxWrap.StdVector(x), 1, 0,
-		                                   MyOpenFHE.CxxWrap.StdLib.SharedPtr{MyOpenFHE.ILDCRTParams{MyOpenFHE.ubint{UInt64}}}(),
-						   0)
+        MyOpenFHE.CxxWrap.StdLib.SharedPtr{MyOpenFHE.ILDCRTParams{MyOpenFHE.ubint{UInt64}}}(),
+        0)
     @show MyOpenFHE.SetLength(ptxt[], length(x))
     @show ep = MyOpenFHE.GetEncodingParams(ptxt[])
     @show batch_size= Int(MyOpenFHE.GetBatchSize(ep[]))
