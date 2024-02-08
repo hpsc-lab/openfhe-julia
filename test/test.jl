26b464c21747af7a178a4eabc87907a6f5894e27
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-02-08 11:08:54 +0100

    Add GetEncodingParameters/GetEncodingParams to CryptoObject and Plaintext (#22)
    * GetEncodingParams
    
    * try to fix
    
    * add test GetEncodingParams Plaintext
    
    * Update src/cryptoobject.cpp
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>
    
    * Update test/test.jl
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>
    
    * Update test/test.jl
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>
    
    * Update test/test.jl
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>
    
    ---------
    
    Co-authored-by: Arseniy Kholod <kholod@rocinante.mi.uni-koeln.de>
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- test/test.jl
+++ test/test.jl
@@ -8,4 +8,14 @@ using Test
     @show batch_size = Int(MyOpenFHE.GetBatchSize(ep[]))
 
     @test batch_size == 8192
+
+    x = [0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0]
+    @show ptxt = MyOpenFHE.MakeCKKSPackedPlaintext(cc[], MyOpenFHE.CxxWrap.StdVector(x), 1, 0,
+        MyOpenFHE.CxxWrap.StdLib.SharedPtr{MyOpenFHE.ILDCRTParams{MyOpenFHE.ubint{UInt64}}}(),
+        0)
+    @show MyOpenFHE.SetLength(ptxt[], length(x))
+    @show ep = MyOpenFHE.GetEncodingParams(ptxt[])
+    @show batch_size = Int(MyOpenFHE.GetBatchSize(ep[]))
+
+    @test batch_size == 8192
 end
