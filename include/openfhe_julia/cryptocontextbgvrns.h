53abfb193e317264f22ca9efee6c7dea4880ac5d
Michael Schlottke-Lakemper <michael@sloede.com>
2024-02-12 16:43:29 +0100

    Add missing header file

---
+++ include/openfhe_julia/cryptocontextbgvrns.h
@@ -0,0 +1,8 @@
+#ifndef OPENFHE_JULIA_CRYPTOCONTEXTBGVRNS_H_
+#define OPENFHE_JULIA_CRYPTOCONTEXTBGVRNS_H_
+
+#include "jlcxx/jlcxx.hpp"
+
+void wrap_CryptoContextBGVRNS(jlcxx::Module& mod);
+
+#endif // ifndef OPENFHE_JULIA_CRYPTOCONTEXTBGVRNS_H_
