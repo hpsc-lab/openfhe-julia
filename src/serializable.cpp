389b492e9dcaceec1b52d9f4896d2b6f7ccc1a8c
Michael Schlottke-Lakemper <michael@sloede.com>
2024-02-13 07:48:44 +0100

    Fix comment

--- src/serializable.cpp
+++ src/serializable.cpp
@@ -2,6 +2,8 @@
 #include "openfhe.h"
 
 void wrap_Serializable(jlcxx::Module& mod) {
+  // Source: <OpenFHE>/src/core/include/utils/serializable.h
+  // Note: This file wraps all functions (except constructors) that are defined in OpenFHE
   mod.add_type<lbcrypto::Serializable>("Serializable")
     .method("SerializedObjectName", &lbcrypto::Serializable::SerializedObjectName);
 }
