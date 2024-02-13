a348475fb794f6e81934d08a7a1e641ad2d326a7
Michael Schlottke-Lakemper <michael@sloede.com>
2024-02-13 05:57:20 +0100

    Add nearly all PlaintextImpl methods

--- src/plaintextimpl.cpp
+++ src/plaintextimpl.cpp
@@ -2,12 +2,27 @@
 #include "openfhe.h"
 
 void wrap_PlaintextImpl(jlcxx::Module& mod) {
+  // Source: <OpenFHE>/src/pke/include/encoding/plaintext.h
+  // Note: This file already contains all functions (except constructors) that are defined
+  //       in OpenFHE. For feature completeness, one only needs to implement the currently
+  //       commented functions.
   mod.add_type<lbcrypto::PlaintextImpl>("PlaintextImpl")
+    .method("GetEncodingType", &lbcrypto::PlaintextImpl::GetEncodingType)
     .method("GetScalingFactor", &lbcrypto::PlaintextImpl::GetScalingFactor)
     .method("SetScalingFactor", &lbcrypto::PlaintextImpl::SetScalingFactor)
+    .method("GetScalingFactorInt", &lbcrypto::PlaintextImpl::GetScalingFactorInt)
+    .method("SetScalingFactorInt", &lbcrypto::PlaintextImpl::SetScalingFactorInt)
+    .method("GetSchemeID", &lbcrypto::PlaintextImpl::GetSchemeID)
     .method("IsEncoded", &lbcrypto::PlaintextImpl::IsEncoded)
     .method("GetEncodingParams", &lbcrypto::PlaintextImpl::GetEncodingParams)
+    .method("Encode", &lbcrypto::PlaintextImpl::Encode)
+    .method("Decode", &lbcrypto::PlaintextImpl::Decode)
+    .method("LowBound", &lbcrypto::PlaintextImpl::LowBound)
+    .method("HighBound", &lbcrypto::PlaintextImpl::HighBound)
+    // .method("SetFormat", &lbcrypto::PlaintextImpl::SetFormat)
+    // .method("GetElement", &lbcrypto::PlaintextImpl::GetElement)
     .method("GetElementRingDimension", &lbcrypto::PlaintextImpl::GetElementRingDimension)
+    .method("GetElementModulus", &lbcrypto::PlaintextImpl::GetElementModulus)
     .method("GetLength", &lbcrypto::PlaintextImpl::GetLength)
     .method("SetLength", &lbcrypto::PlaintextImpl::SetLength)
     .method("GetNoiseScaleDeg", &lbcrypto::PlaintextImpl::GetNoiseScaleDeg)
@@ -21,5 +36,10 @@ void wrap_PlaintextImpl(jlcxx::Module& mod) {
     .method("GetStringValue", &lbcrypto::PlaintextImpl::GetStringValue)
     .method("GetCoefPackedValue", &lbcrypto::PlaintextImpl::GetCoefPackedValue)
     .method("GetPackedValue", &lbcrypto::PlaintextImpl::GetPackedValue)
-    .method("GetRealPackedValue", &lbcrypto::PlaintextImpl::GetRealPackedValue);
+    // .method("GetCKKSPackedValue", &lbcrypto::PlaintextImpl::GetCKKSPackedValue)
+    .method("GetRealPackedValue", &lbcrypto::PlaintextImpl::GetRealPackedValue)
+    // .method("SetStringValue", &lbcrypto::PlaintextImpl::SetStringValue)
+    // .method("SetIntVectorValue", &lbcrypto::PlaintextImpl::SetIntVectorValue)
+    .method("CompareTo", &lbcrypto::PlaintextImpl::CompareTo);
+    // .method("PrintValue", &lbcrypto::PlaintextImpl::PrintValue);
 }
