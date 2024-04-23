6fa08665122c18e2d80e94f08ac28672ae8ff6f7
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-04-23 20:46:33 +0300

    Update fheckksrns.cpp

--- src/fheckksrns.cpp
+++ src/fheckksrns.cpp
@@ -4,9 +4,14 @@
 void wrap_FHECKKSRNS(jlcxx::Module& mod) {
   mod.add_type<lbcrypto::FHECKKSRNS>("FHECKKSRNS")
   .method("EvalBootstrapSetup", &lbcrypto::FHECKKSRNS::EvalBootstrapSetup)
+  .method("EvalLinearTransformPrecompute", 
+      static_cast<std::vector<lbcrypto::ConstPlaintext>
+                  (lbcrypto::FHECKKSRNS::*)(const lbcrypto::CryptoContextImpl<lbcrypto::DCRTPoly>&,
+                      const std::vector<std::vector<std::complex<double>>>&,
+                      double, uint32_t)>(&lbcrypto::FHECKKSRNS::EvalLinearTransformPrecompute))
   .method("EvalLinearTransform", &lbcrypto::FHECKKSRNS::EvalLinearTransform)
   .module().method("GetBootstrapDepth",
       static_cast<uint32_t
-                  (*)(const std::vector<uint32_t>&,
+                  (lbcrypto::FHECKKSRNS::*)(const std::vector<uint32_t>&,
                   lbcrypto::SecretKeyDist)>(&lbcrypto::FHECKKSRNS::GetBootstrapDepth));
 }
