0a03210b1af0c0cc571eb88a6334633215c6f7de
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-04-23 17:48:06 +0300

    Update fheckksrns.cpp

--- src/fheckksrns.cpp
+++ src/fheckksrns.cpp
@@ -3,8 +3,11 @@
 
 void wrap_FHECKKSRNS(jlcxx::Module& mod) {
   mod.add_type<lbcrypto::FHECKKSRNS>("FHECKKSRNS")
-  .module().method("GetBootstrapDepth", &lbcrypto::FHECKKSRNS::GetBootstrapDepth)
-  .method("EvalBootstrapSetup", &lbcrypto::FHECKKSRNS::EvalBootstrapSetup)
-  .method("EvalLinearTransformPrecompute", &lbcrypto::FHECKKSRNS::EvalLinearTransformPrecompute)
-  .method("EvalLinearTransform", &lbcrypto::FHECKKSRNS::EvalLinearTransform);
+    .apply<lbcrypto::FHECKKSRNS>([](auto wrapped) {
+        typedef typename decltype(wrapped)::type WrappedT;
+        wrapped.method("EvalBootstrapSetup", &lbcrypto::FHECKKSRNS::EvalBootstrapSetup);
+        wrapped.method("EvalLinearTransformPrecompute", &lbcrypto::FHECKKSRNS::EvalLinearTransformPrecompute);
+        wrapped.method("EvalLinearTransform", &lbcrypto::FHECKKSRNS::EvalLinearTransform);
+        wrapped.module().method("GetBootstrapDepth", &lbcrypto::FHECKKSRNS::GetBootstrapDepth);
+      });
 }
