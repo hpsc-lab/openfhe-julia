d1eb082a837d04559d8d8404d1ea348c65601966
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-08-02 14:18:48 +0300

    Update cryptocontextfactory.cpp

--- src/cryptocontextfactory.cpp
+++ src/cryptocontextfactory.cpp
@@ -5,7 +5,7 @@
 #include "openfhe_julia/jlcxx_parameters.h"
 
 void wrap_CryptoContextFactory(jlcxx::Module& mod) {
-  jlcxx::stl::apply_stl<std::vector<lbcrypto::CryptoContext<lbcrypto::DCRTPoly>>>(mod);
+  //jlcxx::stl::apply_stl<std::vector<lbcrypto::CryptoContext<lbcrypto::DCRTPoly>>>(mod);
   mod.add_type<jlcxx::Parametric<jlcxx::TypeVar<1>>>("CryptoContextFactory")
     .apply<lbcrypto::CryptoContextFactory<lbcrypto::DCRTPoly>>([](auto wrapped) {
     typedef typename decltype(wrapped)::type WrappedT;
@@ -14,6 +14,6 @@ void wrap_CryptoContextFactory(jlcxx::Module& mod) {
     wrapped.module().method("GetContextCount", &WrappedT::GetContextCount);
     //wrapped.module().method("GetContext", &WrappedT::GetContext);
     wrapped.module().method("GetFullContextByDeserializedContext", &WrappedT::GetFullContextByDeserializedContext);    
-    //wrapped.module().method("GetAllContexts", &WrappedT::GetAllContexts);
+    wrapped.module().method("GetAllContexts", &WrappedT::GetAllContexts);
   });
 }
