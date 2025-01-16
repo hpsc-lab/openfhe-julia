a4ed9b4b8e01ec4e5c2464ebe50796977bc7a1fb
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2025-01-16 06:17:16 +0100

    UndefVarError: `StdFill` not defined MacOS  (#77)
    * run ci
    
    * enable tests outside ubuntu
    
    * Revert "enable tests outside ubuntu"
    
    This reverts commit 1bacba2a86e43ccfd1905bd3587dc427627a8059.
    
    * undef ranges
    
    * Update src/openfhe_julia.cpp
    
    ---------
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/openfhe_julia.cpp
+++ src/openfhe_julia.cpp
@@ -29,6 +29,10 @@
 #include "openfhe_julia/cryptocontextfactory.h"
 #include "openfhe_julia/auxiliary.h"
 
+// Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
+#ifdef __APPLE__
+    #undef JLCXX_HAS_RANGES
+#endif
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
   // Enums
