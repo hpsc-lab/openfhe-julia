edcf8cc9b9e05905232271aa772ef86ce08dcf72
Michael Schlottke-Lakemper <michael@sloede.com>
2025-01-16 06:12:39 +0100

    Update src/openfhe_julia.cpp

--- src/openfhe_julia.cpp
+++ src/openfhe_julia.cpp
@@ -29,7 +29,7 @@
 #include "openfhe_julia/cryptocontextfactory.h"
 #include "openfhe_julia/auxiliary.h"
 
-// Until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 is not solved
+// Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
 #ifdef __APPLE__
     #undef JLCXX_HAS_RANGES
 #endif
