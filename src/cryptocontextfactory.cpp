dd757edde718fa5831bc52351a5e42ad5b7a1880
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2025-01-23 18:41:43 +0200

    hopefully final try

--- src/cryptocontextfactory.cpp
+++ src/cryptocontextfactory.cpp
@@ -1,13 +1,14 @@
 #include "jlcxx/jlcxx.hpp"
-#include "jlcxx/stl.hpp"
-#include "openfhe.h"
-
-#include "openfhe_julia/jlcxx_parameters.h"
 
 // Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
 #ifdef __APPLE__
     #undef JLCXX_HAS_RANGES
 #endif
+
+#include "jlcxx/stl.hpp"
+#include "openfhe.h"
+
+#include "openfhe_julia/jlcxx_parameters.h"
 
 void wrap_CryptoContextFactory(jlcxx::Module& mod) {
   jlcxx::stl::apply_stl<lbcrypto::CryptoContext<lbcrypto::DCRTPoly>>(mod);
