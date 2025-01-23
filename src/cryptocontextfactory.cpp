bf19169ecab13d98d52e4fcd13b325a6540e8c19
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2025-01-23 19:26:20 +0100

    `StdFill` is still required... (#80)
    * run ci
    
    * enable tests outside ubuntu
    
    * Revert "enable tests outside ubuntu"
    
    This reverts commit 1bacba2a86e43ccfd1905bd3587dc427627a8059.
    
    * undef ranges
    
    * Update src/openfhe_julia.cpp
    
    * Update VERSION
    
    * Update VERSION
    
    * StdFill is still required...
    
    * Update ci.yml
    
    * Update ci.yml
    
    * new try
    
    * new try
    
    * Update ccparams.cpp
    
    * new try
    
    * Update stl_modified.h
    
    * new try
    
    * new try
    
    * Revert "new try"
    
    This reverts commit 0b401d160c03f09f01e45dfb7d2957bf898dfb7a.
    
    * new try
    
    * next try
    
    * new try
    
    * hopefully final try
    
    * finally disable debugging
    
    ---------
    
    Co-authored-by: Michael Schlottke-Lakemper <michael@sloede.com>

--- src/cryptocontextfactory.cpp
+++ src/cryptocontextfactory.cpp
@@ -1,4 +1,10 @@
 #include "jlcxx/jlcxx.hpp"
+
+// Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
+#ifdef __APPLE__
+    #undef JLCXX_HAS_RANGES
+#endif
+
 #include "jlcxx/stl.hpp"
 #include "openfhe.h"
 
--- src/openfhe_julia.cpp
+++ src/openfhe_julia.cpp
@@ -29,11 +29,6 @@
 #include "openfhe_julia/cryptocontextfactory.h"
 #include "openfhe_julia/auxiliary.h"
 
-// Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
-#ifdef __APPLE__
-    #undef JLCXX_HAS_RANGES
-#endif
-
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
   // Enums
   wrap_PKESchemeFeature(mod);
