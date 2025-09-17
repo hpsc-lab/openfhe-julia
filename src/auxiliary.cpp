5aa0f5331fb27e77e48c9866d77b906f567a3d6c
Max Gruebmeyer <max@gruebmeyer.de>
2025-09-17 18:29:43 +0200

    Removed one method from wrap_auxiliary because the core functionality was set to private

--- src/auxiliary.cpp
+++ src/auxiliary.cpp
@@ -2,12 +2,13 @@
 #include "openfhe.h"
 
 void wrap_auxiliary(jlcxx::Module& mod) {
+  // TODO (GM): Is it fine to remove this? PrintValue is set to protected...
   // Helper function for getting the text representation where only operator<< is defined
-  mod.method("_to_string", [](lbcrypto::Plaintext& pt) {
-      std::ostringstream stream;
-      pt->PrintValue(stream);
-      return stream.str();
-    });
+  // mod.method("_to_string", [](lbcrypto::Plaintext& pt) {
+  //     std::ostringstream stream;
+  //     pt->PrintValue(stream);
+  //     return stream.str();
+  //   });
   mod.method("get_native_int", []() {
       return NATIVEINT;
     });
