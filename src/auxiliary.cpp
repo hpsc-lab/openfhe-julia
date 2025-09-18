70c942f1a0c1a7de1bba158e8e4ed16fb0caf7d2
Max Gruebmeyer <max@gruebmeyer.de>
2025-09-18 11:51:53 +0200

    Replaced PrintValue with << operator

--- src/auxiliary.cpp
+++ src/auxiliary.cpp
@@ -2,13 +2,12 @@
 #include "openfhe.h"
 
 void wrap_auxiliary(jlcxx::Module& mod) {
-  // TODO (GM): Is it fine to remove this? PrintValue is set to protected...
-  // Helper function for getting the text representation where only operator<< is defined
-  // mod.method("_to_string", [](lbcrypto::Plaintext& pt) {
-  //     std::ostringstream stream;
-  //     pt->PrintValue(stream);
-  //     return stream.str();
-  //   });
+// Helper function for getting the text representation where only operator<< is defined
+  mod.method("_to_string", [](lbcrypto::Plaintext& pt) {
+      std::ostringstream stream;
+      stream << pt;
+      return stream.str();
+    });
   mod.method("get_native_int", []() {
       return NATIVEINT;
     });
