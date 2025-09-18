121356486336e144196e923f223def30f284fd84
Max Gruebmeyer <max@gruebmeyer.de>
2025-09-18 15:08:47 +0200

    Readded jlcxx/stl.hpp and commented out JLCXX_HAS_RANGES

--- src/cryptocontextfactory.cpp
+++ src/cryptocontextfactory.cpp
@@ -1,5 +1,11 @@
 #include "jlcxx/jlcxx.hpp"
 
+// Workaround: disable ranges usage in jlcxx until libstdc++ ambiguity is resolved
+//  Not disabling this will result in an error when loading "jlcxx/stl.hpp"
+#undef JLCXX_HAS_RANGES
+
+#include "jlcxx/stl.hpp"
+
 // Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
 #ifdef __APPLE__
     #undef JLCXX_HAS_RANGES
