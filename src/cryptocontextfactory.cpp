229bc2b8f0c9d28a4bb2c894062d0bb39f3e93b5
Max Gruebmeyer <max@gruebmeyer.de>
2025-09-18 15:11:38 +0200

    Removed uneeded code

--- src/cryptocontextfactory.cpp
+++ src/cryptocontextfactory.cpp
@@ -6,11 +6,6 @@
 
 #include "jlcxx/stl.hpp"
 
-// Required until https://github.com/JuliaInterop/CxxWrap.jl/issues/455 has been solved
-#ifdef __APPLE__
-    #undef JLCXX_HAS_RANGES
-#endif
-
 #include "openfhe.h"
 #include "openfhe_julia/jlcxx_parameters.h"
 
